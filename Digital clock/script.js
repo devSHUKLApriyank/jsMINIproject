ef7971f1080c70d94730ac3535d3a173f9f12a83
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-20 16:20:49 +0530

    feat: enhance Digital Clock functionality to display AM/PM and current date

--- Digital clock/script.js
+++ Digital clock/script.js
@@ -1,13 +1,36 @@
 const clock = document.querySelector("#clock")
+const date = document.querySelector("#date")
 
 function currentHour() {
     const now = new Date()
 
-    const hour = String(now.getHours()).padStart(2, "0")
+    let hour = now.getHours();
+
+    let period;
+
+    if (hour >= 12) {
+        period = "PM";
+    } else {
+        period = "AM";
+    }
+
+    if (hour === 0) {
+        hour = 12;
+    }
+
+    if (hour > 12) {
+        hour = hour - 12;
+    }
+
+    hour = String(hour).padStart(2, "0");
+
     const minute = String(now.getMinutes()).padStart(2, "0")
     const second = String(now.getSeconds()).padStart(2, "0")
 
-    clock.textContent = `${hour}:${minute}:${second}`
+
+
+    clock.textContent = `${hour}:${minute}:${second} ${period}`
+
 }
 
 currentHour();
@@ -15,3 +38,16 @@ currentHour();
 setInterval(() => {
     currentHour();
 }, 1000)
+
+
+function currentDate() {
+    const now = new Date()
+
+    const day = String(now.getDate()).padStart(2, "0")
+    const month = String(now.getMonth() + 1).padStart(2, "0")
+    const year = String(now.getFullYear())
+
+    date.textContent = `${day}/${month}/${year}`
+}
+
+currentDate()
